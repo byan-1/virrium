52def99266ab3af2a46064dcdf62b28bcc30babd
Bill Yan <bxyan5@gmail.com>
2019-08-17 08:21:25 +1000

    fixed folder structure

--- node_src/index.ts
+++ node_src/index.ts
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Model } = require('objection');
+const {Model} = require('objection');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const passport = require('passport');
@@ -11,23 +11,19 @@ const errHandler = require('./middleware/errHandler');
 require('./services/passport');
 
 const app = express()
-  .use(bodyParser.json())
-  .use(
-    cookieSession({
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey]
-    })
-  )
-  .use(morgan('combined'))
-  .use(passport.initialize())
-  .use(passport.session())
-  .use(router)
-  .use(errHandler);
+                .use(bodyParser.json())
+                .use(cookieSession(
+                    {maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey]}))
+                .use(morgan('combined'))
+                .use(passport.initialize())
+                .use(passport.session())
+                .use(router)
+                .use(errHandler);
 
 Model.knex(knex);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//Fixes Typescript namespace error
+// Fixes Typescript namespace error
 export {};
